59ed99c9f3aafb7393acb0920529c19e9829738a
VIRU143 <113455603+VIRU143@users.noreply.github.com>
2023-02-07 14:31:23 +0530

    Delete AASHU.js

--- AASHU.js
+++
@@ -1,8 +0,0 @@
-function responsiveNav() {
-    var x = document.getElementsByClassName("navbar")[0];
-    if (x.className === "navbar") {
-        x.className += " responsive";
-    } else {
-        x.className = "navbar";
-    }
-}
